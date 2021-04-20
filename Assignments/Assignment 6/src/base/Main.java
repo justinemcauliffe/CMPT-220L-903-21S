2ccc7bebc520e2f1af6e13a9119487eeb80f0db3
justinemcauliffe <79478004+justinemcauliffe@users.noreply.github.com>
2021-04-20 00:12:13 -0400

    Create Main.java

---
+++ Assignments/Assignment 6/src/base/Main.java
@@ -0,0 +1,7 @@
+package base;
+public class Main {
+    public static void main(String[] args) {
+	    Plant cactus = new Plant("cactus", "succulent", false);
+	    cactus.wateringInstructions();
+    }
+}
