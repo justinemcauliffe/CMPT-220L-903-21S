0e8466435b5283c367c89b07f4a17027afe8f563
justinemcauliffe <79478004+justinemcauliffe@users.noreply.github.com>
2021-04-26 21:40:06 -0400

    Assignment5

--- JupyterNotebooks/Lab6/src/base/Main.java
+++ JupyterNotebooks/Lab6/src/base/Main.java
@@ -1,19 +1,15 @@
 package base;
-// We want to create our own object (lets call it listItem) that will hold a value and another instance of our object that we created.
-// listItem (recommended basic configuration (feel free to change this if you think you need to))
-// value = <Any Data Type> (value in the constructor)
-// listItem = None (value in the constructor)
-// Be able to chain our objects together
-// Chain 4 instances of our list items together
 
 public class Main {
-    public Main(String emotion) {
-        // This constructor has one parameter, name.
-        System.out.println("I am " + emotion );
-    }
+    public static void main(String[] args) {
+        Nodenew Node();
+        Node first = new Node( value: 1);
+        Node second = new Node( value: 2);
+        Node third = new Node( value: 3);
+        Node fourth = new Node( value: 4);
 
-    public static void main(String []args) {
-        // Following statement would create an object myPuppy
-        Main Emotion = new Main( "happy" );
+        first.nextNode = second;
+        second.nextNode = third;
+        third.nextNode = fourth;
     }
 }
